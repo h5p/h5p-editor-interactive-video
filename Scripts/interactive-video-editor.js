a74fec244251e6d44f1d98a911f988488bb7fbbb
Frode Petterson <frode.andre.petterson@gmail.com>
2017-02-02 15:26:15 +0100

    Use new function to get correct position value
    HFP-696

--- Scripts/interactive-video-editor.js
+++ Scripts/interactive-video-editor.js
@@ -987,8 +987,8 @@ H5PEditor.widgets.interactiveVideo = H5PEditor.InteractiveVideo = (function ($) 
   InteractiveVideoEditor.prototype.fit = function ($interaction, interactionParams) {
     var self = this;
 
-    var videoContainer = H5P.DragNBar.getSizeNPosition(self.IV.$videoWrapper[0]);
-    var updated = H5P.DragNBar.fitElementInside($interaction, videoContainer);
+    var sizeNPosition = self.dnb.getElementSizeNPosition($interaction);
+    var updated = H5P.DragNBar.fitElementInside(sizeNPosition);
 
     // Set the updated properties
     var style = {};
